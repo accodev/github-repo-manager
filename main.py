4577c8329da66a2f74f46f335066cd9e419875fb
Marco Accorinti <accosk8@gmail.com>
2023-12-06 18:53:03 +0100

    add first implementation, work in progress

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import logging
+import logging.config
+import argparse
+import asyncio
+from github import Github, Auth
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def main():
+    parser = argparse.ArgumentParser(prog='github-manager')
+    parser.add_argument('-t|--token',
+                        help='GitHub Personal Access Token with the required scopes.',
+                        required=True)
+    parser.add_argument('-u|--url',
+                        help='The url of the enterprise instance of GitHub',
+                        required=False)
+    parser.add_argument('-r|--repos',
+                        help='The repositories on which to apply the action(s)',
+                        nargs='+',
+                        required=True)
+    parser.add_argument('-a|--actions',
+                        help='The actions to apply',
+                        nargs='+',
+                        required=True)
+    args = parser.parse_args()
+
+    auth = Auth.Token(args.token)
+    gh = Github(base_url=args.url if args.url else None, auth=auth)
+
+    tasks = []
+    for repo in args.repos:
+        logging.info(f'Preparing actions for: {repo}')
+        for action in args.actions:
+            logging.info(f'Adding {action}')
+            tasks.append(lambda: action)
+
+    await asyncio.gather(*tasks, return_exceptions=True)
+
+    gh.close()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    try:
+        event_loop = asyncio.get_event_loop()
+        event_loop.run_until_complete(main())
+    except Exception as e:
+        logger.exception('Exception: {e}', e)
