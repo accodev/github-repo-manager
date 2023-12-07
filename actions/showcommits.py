e3ebbd1ab68a76761ba766e7d71661ebb58517f8
Marco Accorinti <accosk8@gmail.com>
2023-12-07 18:35:17 +0100

    rename SampleAction to ShowCommits, fix Result return values

--- actions/showcommits.py
+++ actions/showcommits.py
@@ -8,17 +8,17 @@ from github.GithubException import *
 logger = logging.getLogger(__name__)
 
 
-class SampleAction(Action):
+class ShowCommits(Action):
     async def run(self) -> [Result, None]:
         try:
             commits = self.repo.get_commits()
             for commit in commits:
                 logging.debug(commit)
-            return Result('Ok', 0)
+            return Result(self.name, 'Ok', 0)
         except GithubException:
             logger.exception('GithubException')
-            return Result('Exception', 1)
+            return Result(self.name, 'Exception', 1)
 
 
 def build(name: str, repo):
-    return SampleAction(name, repo)
+    return ShowCommits(name, repo)
